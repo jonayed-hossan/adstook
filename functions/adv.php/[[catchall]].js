4d9e27666ddd01464bd998543e717de7cf417837
Zunaid Hossan Gazi <me@zuna.id>
2024-08-17 16:01:32 +0600

    cf pages

--- functions/adv.php/[[catchall]].js
+++ functions/adv.php/[[catchall]].js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
  console.log("adv log");
-  return new Response(JSON.stringify(context.params.catchall) + "hello for adv.php")
+  return new Response(JSON.stringify(context) + "hello for adv.php")
 }
